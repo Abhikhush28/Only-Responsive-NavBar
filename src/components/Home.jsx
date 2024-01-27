e72c81ebc2ee669350c913cd0c0f03b8eb3b1347
Abhikhush28 <sahuabhikhush@gmail.com>
2024-01-27 14:57:45 +0530

    Responsive NavBar

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text'>React NavBar</h1>
+      <span>Difficulty Level: 0;</span>
+    </div>
+  )
+}
+
+export default Home;
